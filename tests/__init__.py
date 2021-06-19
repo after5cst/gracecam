c65f5e6221d7d188eaa7370ec0918650b5dec6a6
Joe Marley <joe.marley@live.com>
2021-06-19 10:36:59 -0600

    Initial package structure

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+'''
+gracecam: tests module.
+
+Meant for use with py.test.
+Organize tests into files, each named xxx_test.py
+Read more here: http://pytest.org/
+
+Copyright 2021, Joe Marley
+Licensed under MIT
+'''
---
+++ tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+gracecam: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2021, Joe Marley
+Licensed under MIT
+'''
+
+def test_example():
+    assert True
